5aaa9c3b1232b8b6c72e76b22192573f1d68175e
ved015 <vedant.04.mahajan@gmail.com>
2026-04-17 16:28:55 +0530

    fix cors

--- src/api/app.py
+++ src/api/app.py
@@ -93,7 +93,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://xmem-eight.vercel.app", "https://xmem.in"],
+        allow_origins=settings.cors_origins,
         # allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],

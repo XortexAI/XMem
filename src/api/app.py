9f91afdf1684cdf7d0fdd1dbb5fb89824d1c2093
Ishaan Gupta <ishaankone@gmail.com>
2026-04-17 16:06:21 +0530

    modify security issues

--- src/api/app.py
+++ src/api/app.py
@@ -94,7 +94,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
+        # allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
         expose_headers=["X-Request-ID", "X-Response-Time-Ms", "X-RateLimit-Remaining"],
